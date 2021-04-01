eb60e5bc905ac342312cf347b75b2355db4eac05
Ivan Vorisov <tvadmix@gmail.com>
2021-04-01 22:18:55 +0300

    heroku conf

--- src/main/java/com/creation/App.java
+++ src/main/java/com/creation/App.java
@@ -1,9 +1,7 @@
 package com.creation;
 
-import com.creation.filter.url.RegionFilter;
-import com.creation.filter.url.SortProductsFilter;
-import com.creation.filter.url.UrlFilter;
-import com.creation.filter.url.price.PriceMaxFilter;
+import org.telegram.telegrambots.*;
+
 import org.apache.log4j.Logger;
 
 import java.util.Timer;
@@ -14,11 +12,10 @@ public class App {
 
 
   public static void main(String[] args) {
-    //ApiContextInitializer.init();
+    ApiContextInitializer.init();
     Task task = new AvitoProductsTask();
     MainBot mainBot = new MainBot(task);
     mainBot.setToken("1724743407:AAGMX1kuvCZEz40hnC6m5IqTfPgdGLY-3_g");
     mainBot.connect();
-
   }
 }

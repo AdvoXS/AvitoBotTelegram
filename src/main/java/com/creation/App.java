cd16ed4bf6f18bdf95638cccf0b0dee410a714ea
Ivan Vorisov <tvadmix@gmail.com>
2021-04-02 22:24:55 +0300

    revert downgrade depend telegrambot

--- src/main/java/com/creation/App.java
+++ src/main/java/com/creation/App.java
@@ -2,13 +2,11 @@ package com.creation;
 
 
 import org.apache.log4j.Logger;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 public class App {
   private static final Logger log = Logger.getLogger(App.class);
 
   public static void main(String[] args) {
-    ApiContextInitializer.init();
     Task task = new AvitoProductsTask();
     MainBot mainBot = new MainBot(task);
     mainBot.setUserName("AvitoVideocardsParserBot");
--- src/main/java/com/creation/MainBot.java
+++ src/main/java/com/creation/MainBot.java
@@ -95,7 +95,7 @@ public class MainBot extends TelegramLongPollingBot {
 
   public void connect(){
     try {
-      TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+      TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
       telegramBotsApi.registerBot(this);
       SystemMessage.putDebug(log,"TelegramAPI started. Look for messages");
 
@@ -109,6 +109,9 @@ public class MainBot extends TelegramLongPollingBot {
         return;
       }
       connect();
+    } catch (TelegramApiException e) {
+      SystemMessage.putError(log, e.getMessage());
+      e.printStackTrace();
     }
   }
 
